fb7908304bb577a11fe7b4ac644708bbf7f5d5a9
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-02-12 12:23:39 -0300

    [Aula 02] Regras de fisica no personagem Jogador

--- Assets/Scripts/ControlaJogador.cs
+++ Assets/Scripts/ControlaJogador.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ControlaJogador : MonoBehaviour
 {
     public float Velocidade = 10;
+    Vector3 direcao;
 
     // Update is called once per frame
     void Update()
@@ -12,10 +13,17 @@ public class ControlaJogador : MonoBehaviour
         float eixoX = Input.GetAxis("Horizontal");
         float eixoZ = Input.GetAxis("Vertical");
 
-        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
-
-        transform.Translate(direcao * Velocidade * Time.deltaTime);
+        direcao = new Vector3(eixoX, 0, eixoZ);
 
         GetComponent<Animator>().SetBool("Movendo", (direcao != Vector3.zero));
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody rigidbodi = GetComponent<Rigidbody>();
+        rigidbodi.MovePosition(
+            rigidbodi.position +
+            (direcao * Velocidade * Time.deltaTime)
+            );
     }
 }
