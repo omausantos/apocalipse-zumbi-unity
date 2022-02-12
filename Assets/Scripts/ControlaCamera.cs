f24806aed006bdcd01691ca89cb217ef06980959
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-02-12 10:13:00 -0300

    [Aula 02] Inclusão de camera fixa no Jogador

---
+++ Assets/Scripts/ControlaCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaCamera : MonoBehaviour
+{
+    public GameObject Jogador;
+    Vector3 distanciaCameraJogador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanciaCameraJogador = transform.position - Jogador.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Jogador.transform.position + distanciaCameraJogador;
+        
+    }
+}
