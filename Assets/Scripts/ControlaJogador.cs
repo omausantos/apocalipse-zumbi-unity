b10f0e185cf9e80b88166329dd6145106f353c74
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-02-18 23:04:26 -0300

    [M2A1] Inclusão de pontução para vida do Jogador

--- Assets/Scripts/ControlaJogador.cs
+++ Assets/Scripts/ControlaJogador.cs
@@ -9,9 +9,9 @@ public class ControlaJogador : MonoBehaviour
     private Vector3 direcao;
     public LayerMask mascaraChao;
     public GameObject TextoGameOver;
-    public bool Vivo = true;
     private Rigidbody rigidbodyJogador;
     private Animator animatorJogador;
+    public int Vida = 100;
 
 
     private void Start()
@@ -31,7 +31,7 @@ public class ControlaJogador : MonoBehaviour
 
         animatorJogador.SetBool("Movendo", (direcao != Vector3.zero));
 
-        if (Vivo == false)
+        if (estaMorto())
         {
             if (Input.GetButtonDown("Fire1"))
             {
@@ -63,5 +63,20 @@ public class ControlaJogador : MonoBehaviour
             rigidbodyJogador.MoveRotation(novaRotacao);
         }
         
+    }
+
+    public void TomarDano(int dano)
+    {
+        Vida -= dano;
+        if(estaMorto())
+        {
+            Time.timeScale = 0;
+            TextoGameOver.SetActive(true);            
+        }
+    }
+
+    bool estaMorto()
+    {
+        return Vida <= 0 ? true : false;
     }
 }
