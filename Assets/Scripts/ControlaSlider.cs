64a00e266a2c81bc0f6c5327dba750c86abd0989
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-02-19 23:26:38 -0300

    [M2A1] Estilizando Slider de Vida

--- Assets/Scripts/ControlaSlider.cs
+++ Assets/Scripts/ControlaSlider.cs
@@ -13,6 +13,7 @@ public class ControlaSlider : MonoBehaviour
     {
         scriptControlaJogador = GameObject.FindWithTag("Jogador").GetComponent<ControlaJogador>();
         SliderVidaJogador.maxValue = scriptControlaJogador.Vida;
+        AtualizarSliderVidaJogador();
     }
 
     // Update is called once per frame
