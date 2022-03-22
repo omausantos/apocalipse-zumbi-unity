1e6c3b23c0c5d8fe418f5f8ba7a0220db4097a2e
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-03-22 09:17:26 -0300

    [M2A5] Nova Interface de Game Over

--- Assets/Scripts/ControlaSlider.cs
+++ Assets/Scripts/ControlaSlider.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ControlaSlider : MonoBehaviour
 {
     private ControlaJogador scriptControlaJogador;
     public Slider SliderVidaJogador;
+    public GameObject PainelGameOver;
 
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
         scriptControlaJogador = GameObject.FindWithTag("Jogador").GetComponent<ControlaJogador>();
         SliderVidaJogador.maxValue = scriptControlaJogador.StatusJogador.Vida;
         AtualizarSliderVidaJogador();
@@ -19,5 +22,17 @@ public class ControlaSlider : MonoBehaviour
     public void AtualizarSliderVidaJogador()
     {
         SliderVidaJogador.value = scriptControlaJogador.StatusJogador.Vida;
+    }
+
+    public void GameOver()
+    {
+        PainelGameOver.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Reiniciar()
+    {
+        SceneManager.LoadScene("game");
+        PainelGameOver.SetActive(false);
     }
 }
