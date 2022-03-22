db31e31c7a7ccc376e991203c6f2402c3238a10c
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-03-21 21:51:37 -0300

    [M2A4] Zumbi em Local Aleatório

--- Assets/Scripts/GeradorZumbi.cs
+++ Assets/Scripts/GeradorZumbi.cs
@@ -7,22 +7,36 @@ public class GeradorZumbi : MonoBehaviour
     public GameObject Zumbi;
     float ContadorTempo = 0;
     public float TempoGerarZumbi = 1;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public LayerMask LayerZumbi;
 
     // Update is called once per frame
     void Update()
     {
         ContadorTempo += Time.deltaTime;
-        if(ContadorTempo >= TempoGerarZumbi)
+        if (ContadorTempo >= TempoGerarZumbi)
         {
-            Instantiate(Zumbi, transform.position, transform.rotation);
+            GerarZumbi();
             ContadorTempo = 0;
         }
+    }
+
+    void GerarZumbi()
+    {
+        Vector3 posicaoCriarZumbi = PosicaoAleatoria();
+        Collider[] colisores = Physics.OverlapSphere(posicaoCriarZumbi, 1, LayerZumbi);
+        if(colisores.Length > 0)
+        {
+            posicaoCriarZumbi = PosicaoAleatoria();
+            colisores = Physics.OverlapSphere(posicaoCriarZumbi, 1, LayerZumbi);
+        }
+        Instantiate(Zumbi, posicaoCriarZumbi, transform.rotation);
+    }
+
+    Vector3 PosicaoAleatoria()
+    {
+        Vector3 posicao = Random.insideUnitSphere * 3;
+        posicao += transform.position;
+        posicao.y = transform.position.y;
+        return posicao;
     }
 }
